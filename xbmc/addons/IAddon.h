622324559edb7b77a32470169fe41888d4b430f5
spiff <spiff@xbmc.org>
2011-02-13 20:25:15 +0100

    added: support audio codecs as add-ons

--- xbmc/addons/IAddon.h
+++ xbmc/addons/IAddon.h
@@ -48,6 +48,7 @@ namespace ADDON
     ADDON_REPOSITORY,
     ADDON_WEB_INTERFACE,
     ADDON_SERVICE,
+    ADDON_AUDIOCODEC,
     ADDON_VIDEO, // virtual addon types
     ADDON_AUDIO,
     ADDON_IMAGE,
